aa5e25ca22347593d54cca99ddce4e2c05e572f0
Ninja <jiachenning@gmail.com>
2017-06-28 12:53:23 +0800

    Config log dir

---
+++ RESTFUL/start_stop_memcached_script.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+# source from https://gist.github.com/tinogomes/447191
+# BASE=/tmp
+LOG_DIR=log
+
+PID=$LOG_DIR/app.pid
+LOG=$LOG_DIR/app.log
+ERROR=$LOG_DIR/app-error.log
+
+PORT=8090
+LISTEN_IP='0.0.0.0'
+MEM_SIZE=4
+# CMD='memcached'
+# COMMAND="$CMD -p $PORT -l $LISTEN_IP -m $MEM_SIZE -v"
+CMD='mvn'
+COMMAND="$CMD spring-boot:run"
+
+USR=user
+
+status() {
+    echo
+    echo "==== Status"
+
+    if [ -f $PID ]
+    then
+        echo
+        echo "Pid file: $( cat $PID ) [$PID]"
+        echo
+        ps -ef | grep -v grep | grep $( cat $PID )
+    else
+        echo
+        echo "No Pid file"
+    fi
+}
+
+start() {
+    if [ -f $PID ]
+    then
+        echo
+        echo "Already started. PID: [$( cat $PID )]"
+    else
+        echo "==== Start"
+        touch $PID
+        if nohup $COMMAND >>$LOG 2>&1 &
+        then echo $! >$PID
+             echo "Done."
+             echo "$(date '+%Y-%m-%d %X'): START" >>$LOG
+        else echo "Error... "
+             /bin/rm $PID
+        fi
+    fi
+}
+
+kill_cmd() {
+    SIGNAL=""; MSG="Killing "
+    while true
+    do
+        LIST=`ps -ef | grep -v grep | grep $CMD | grep -w $USR | awk '{print $2}'`
+        if [ "$LIST" ]
+        then
+            echo; echo "$MSG $LIST" ; echo
+            echo $LIST | xargs kill $SIGNAL
+            sleep 2
+            SIGNAL="-9" ; MSG="Killing $SIGNAL"
+            if [ -f $PID ]
+            then
+                /bin/rm $PID
+            fi
+        else
+           echo; echo "All killed..." ; echo
+           break
+        fi
+    done
+}
+
+stop() {
+    echo "==== Stop"
+
+    if [ -f $PID ]
+    then
+        if kill $( cat $PID )
+        then echo "Done."
+             echo "$(date '+%Y-%m-%d %X'): STOP" >>$LOG
+        fi
+        /bin/rm $PID
+        kill_cmd
+    else
+        echo "No pid file. Already stopped?"
+    fi
+}
+
+case "$1" in
+    'start')
+            start
+            ;;
+    'stop')
+            stop
+            ;;
+    'restart')
+            stop ; echo "Sleeping..."; sleep 1 ;
+            start
+            ;;
+    'status')
+            status
+            ;;
+    *)
+            echo
+            echo "Usage: $0 { start | stop | restart | status }"
+            echo
+            exit 1
+            ;;
+esac
+
+exit 0
