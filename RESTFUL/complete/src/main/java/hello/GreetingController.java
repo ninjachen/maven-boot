cdda59a03cc9129b13713d218522ea8a248a2ecf
Ninja Chen <jiachenning@gmail.com>
2017-06-28 05:08:06 +0000

    server commit

--- RESTFUL/complete/src/main/java/hello/GreetingController.java
+++ RESTFUL/complete/src/main/java/hello/GreetingController.java
@@ -4,12 +4,14 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.lang.Exception;
 
 @RestController
 public class GreetingController {
@@ -25,11 +27,11 @@ public class GreetingController {
 
     @RequestMapping("/online_test")
 	public String greeting() {
-		File file = new ClassPathResource("stations.json").getFile();
         String content;
         try {
+		File file = new ClassPathResource("stations.json").getFile();
             content = new Scanner(file).useDelimiter("\\Z").next();
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             content = "empty file";
         }
